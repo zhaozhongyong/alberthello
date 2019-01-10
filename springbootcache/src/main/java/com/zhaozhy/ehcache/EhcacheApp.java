cfffb0d7e22064d71501e1cf9bd1dde9483a6d27
zhaozhongyong <zhongyong@qq.com>
2019-01-10 08:49:08 +0800

    ehcache

---
+++ springbootcache/src/main/java/com/zhaozhy/ehcache/EhcacheApp.java
@@ -0,0 +1,14 @@
+package com.zhaozhy.ehcache;/*
+ * @author zzy
+ *
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcacheApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EhcacheApp.class, args);
+    }
+}
