295fbf60a002d111248a6e66ae50d561f28cfc21
zhaozhongyong <zhongyong@qq.com>
2019-01-28 14:20:35 +0800

    redis

---
+++ springboot-redis/src/main/java/com/zhaozhy/redis/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package com.zhaozhy.redis.config;/*
+ * @author zzy
+ *
+ */
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis.pool")
+    public JedisPoolConfig jedisPoolConfig() {
+        return new JedisPoolConfig();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis")
+    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        RedisTemplate<String,Object> redisTemplate=new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
---
+++ springboot-redis/src/test/java/com/zhaozhy/redis/test/RedisTest.java
@@ -0,0 +1,32 @@
+package com.zhaozhy.redis.test;/*
+ * @author zzy
+ *
+ */
+
+import com.zhaozhy.redis.RedisApp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = RedisApp.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    public void testSet() {
+        this.redisTemplate.opsForValue().set("idea", "idea测试");
+    }
+
+    @Test
+    public void testGet() {
+        String value = (String) this.redisTemplate.opsForValue().get("idea");
+        System.out.println(value);
+    }
+
+}
